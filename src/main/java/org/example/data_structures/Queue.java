c1c5ad378f17028a5412a669dd259b027e63758e
aftab <sk.aftab1999@gmail.com>
2022-12-28 22:00:04 +0530

    Implement Queue

---
+++ src/main/java/org/example/data_structures/Queue.java
@@ -0,0 +1,63 @@
+package org.example.data_structures;
+
+import org.example.exceptions.QueueUnderflowException;
+
+import java.util.Arrays;
+
+public class Queue {
+    private Integer[] data = new Integer[10];
+    private Integer tail = -1;
+
+    public void add(Integer element) {
+        if (isFull()) {
+            data = Arrays.copyOf(data, this.data.length + 11);
+        }
+
+        this.data[++tail] = element;
+    }
+
+    public Integer remove() throws Exception {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        }
+
+        Integer result = this.data[0];
+
+        for (int i = 0; i < tail; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+
+        this.tail--;
+        return result;
+    }
+
+    public Boolean isEmpty() {
+        return this.tail.equals(-1);
+    }
+
+    public Boolean isFull() {
+        return this.size() == data.length;
+    }
+
+    public Integer size() {
+        return this.tail + 1;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        }
+
+        StringBuilder result = new StringBuilder("[");
+        int i = 0;
+
+        while (i < this.tail) {
+            result.append(this.data[i]).append(", ");
+            i++;
+        }
+
+        result.append(this.data[i]).append("]");
+        return result.toString();
+    }
+}
